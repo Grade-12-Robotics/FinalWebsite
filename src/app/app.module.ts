b878563dc211d6bd0c088010eb943256eb2a8a52
mat4539 <mborg0020@gmail.com>
2020-05-13 10:37:12 -0400

    set up roughting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +8,12 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { Esp8266Component } from './esp8266/esp8266.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'esp8266', component: Esp8266Component },
+];
 
 @NgModule({
   declarations: [
@@ -18,7 +25,8 @@ import { Esp8266Component } from './esp8266/esp8266.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes) 
   ],
   providers: [],
   bootstrap: [AppComponent]
